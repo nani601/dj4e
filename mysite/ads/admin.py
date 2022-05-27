026ff7af68452e3ee9b9c18bf92bd64b40dd327a
nani601 <nani601@outlook.com>
2022-05-27 14:41:01 +0800

    C4_Week2

--- mysite/ads/admin.py
+++ mysite/ads/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from ads.models import Ad
 
+
+# Define the PicAdmin class
+class AdAdmin(admin.ModelAdmin):
+    exclude = ('picture', 'content_type')
+
+
 # Register your models here.
-admin.site.register(Ad)
+admin.site.register(Ad, AdAdmin)
